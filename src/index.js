691ce038a80a3bff59c8730079c589a2027b26b4
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-18 15:56:29 -0700

    CLEANUP

--- src/index.js
+++ src/index.js
@@ -2,64 +2,6 @@
 var js2smt2 = require('js2smt2');
 var utils = require('../src/utils');
 
-function astRemoveNode(ast, pred) {
-  function filter(ast) {
-    if (!pred(ast)) {
-      return null;
-    }
-
-    return Object.keys(ast).reduce(function(prev, k) {
-      if (Array.isArray(ast[k])) {
-        prev[k] = ast[k]
-          .map(function(elem) {return filter(elem);})
-          .filter(function(elem) {return elem !== null});
-      } else if (typeof ast[k] === 'object' && ast[k] !== null) {
-        var t = filter(ast[k]);
-        if (t !== null) {
-          prev[k] = t;
-        }
-      } else {
-        prev[k] = ast[k];
-      }
-      return prev;
-    }, {});
-  }
-
-  return filter(ast);
-}
-
-function astToJS(ast) {
-  if (ast.type === 'Identifier') {
-    return ast.name;
-  } else if (ast.type === 'UnaryExpression') {
-    return `(${ast.operator} ${astToJS(ast.argument)})`;
-  } else if (ast.type === 'BinaryExpression') {
-    return `(${astToJS(ast.left)} ${ast.operator} ${astToJS(ast.right)})`;
-  } else if (ast.type === 'LogicalExpression') {
-    return `(${astToJS(ast.left)} ${ast.operator} ${astToJS(ast.right)})`;
-  } else if (ast.type === 'ExpressionStatement') {
-    return `${astToJS(ast.expression)}`;
-  } else if (ast.type === 'Literal') {
-    return `${typeof ast.value === 'string' ? JSON.stringify(ast.value) : ast.value}`;
-  } else if (ast.type === 'ReturnStatement') {
-    return `return ${astToJS(ast.argument)}`;
-  } else if (ast.type === 'BlockStatement') {
-    return ast.body.map(function(b) {return `${astToJS(b)};`;}).join(' ');
-  } else if (ast.type === 'IfStatement') {
-    return `if (${astToJS(ast.test)}) { ${astToJS(ast.consequent)} } else { ${astToJS(ast.alternate)} }`;
-  } else if (ast.type === 'MemberExpression') {
-    return `${astToJS(ast.object)}["${astToJS(ast.property)}"]`;
-  } else if (ast.type === 'Property') {
-    return `"${astToJS(ast.key)}": ${astToJS(ast.value)}`;
-  } else if (ast.type === 'ObjectExpression') {
-    return `{${ast.properties.map(function (property) {return astToJS(property);}).join(', ')}}`;
-  } else if (ast.type === 'Program') {
-    return ast.body.map(function (b) {return astToJS(b);}).join(' ');
-  } else {
-    throw new Error(`Invalid input ast=${JSON.stringify(ast)}`);
-  }
-}
-
 function subsituteVariables(ast, variableMap) {
   function subsitute(ast, varMap) {
     if (ast.type === 'Identifier') {
@@ -100,34 +42,36 @@ function subsituteVariables(ast, variableMap) {
   return subsitute(ast, variableMap);
 }
 
-function extractVariables(ast) {
-  function extract(ast, vars) {
-    if (ast.type === 'Identifier') {
-      return vars.slice(0).concat(ast.name);
-    } else if (ast.type === 'MemberExpression') {
-      var objVars = extractVariables(ast.object, vars);
-      var propVars = !ast.property.computed
-        ? [] : extractVariables(ast.property, vars);
-      return objVars.concat(propVars);
-    }
-
-    return Object.keys(ast).reduce(function(prev, k) {
-      if (Array.isArray(ast[k])) {
-        return prev.concat(
-          ast[k]
-            .map(function(elem) {return extract(elem, vars);})
-            .reduce(function (prev, arr) { return prev.concat(arr); }, vars)
-        );
-      } else if (typeof ast[k] === 'object' && ast[k] !== null) {
-        return prev.concat(extract(ast[k], vars));
-      } else {
-        return prev;
-      }
-    }, []);
+function astToJS(ast) {
+  if (ast.type === 'Identifier') {
+    return ast.name;
+  } else if (ast.type === 'UnaryExpression') {
+    return `(${ast.operator} ${astToJS(ast.argument)})`;
+  } else if (ast.type === 'BinaryExpression') {
+    return `(${astToJS(ast.left)} ${ast.operator} ${astToJS(ast.right)})`;
+  } else if (ast.type === 'LogicalExpression') {
+    return `(${astToJS(ast.left)} ${ast.operator} ${astToJS(ast.right)})`;
+  } else if (ast.type === 'ExpressionStatement') {
+    return `${astToJS(ast.expression)}`;
+  } else if (ast.type === 'Literal') {
+    return `${typeof ast.value === 'string' ? JSON.stringify(ast.value) : ast.value}`;
+  } else if (ast.type === 'ReturnStatement') {
+    return `return ${astToJS(ast.argument)}`;
+  } else if (ast.type === 'BlockStatement') {
+    return ast.body.map(function(b) {return `${astToJS(b)};`;}).join(' ');
+  } else if (ast.type === 'IfStatement') {
+    return `if (${astToJS(ast.test)}) { ${astToJS(ast.consequent)} } else { ${astToJS(ast.alternate)} }`;
+  } else if (ast.type === 'MemberExpression') {
+    return `${astToJS(ast.object)}["${astToJS(ast.property)}"]`;
+  } else if (ast.type === 'Property') {
+    return `"${astToJS(ast.key)}": ${astToJS(ast.value)}`;
+  } else if (ast.type === 'ObjectExpression') {
+    return `{${ast.properties.map(function (property) {return astToJS(property);}).join(', ')}}`;
+  } else if (ast.type === 'Program') {
+    return ast.body.map(function (b) {return astToJS(b);}).join(' ');
+  } else {
+    throw new Error(`Invalid input ast=${JSON.stringify(ast)}`);
   }
-
-  var vars = extract(ast, []);
-  return vars.filter(function(v, i){ return vars.indexOf(v) >= i; });
 }
 
 function hasIdentifier(tree) {
@@ -244,9 +188,37 @@ function makeResidual(transAst, paramMap, trace) {
       }, {});
     }
   });
-
-  // var subbedSubAst = subsituteVariables(subAst, paramMap);
   return subAst;
+}
+
+function extractVariables(ast) {
+  function extract(ast, vars) {
+    if (ast.type === 'Identifier') {
+      return vars.slice(0).concat(ast.name);
+    } else if (ast.type === 'MemberExpression') {
+      var objVars = extractVariables(ast.object, vars);
+      var propVars = !ast.property.computed
+        ? [] : extractVariables(ast.property, vars);
+      return objVars.concat(propVars);
+    }
+
+    return Object.keys(ast).reduce(function(prev, k) {
+      if (Array.isArray(ast[k])) {
+        return prev.concat(
+          ast[k]
+            .map(function(elem) {return extract(elem, vars);})
+            .reduce(function (prev, arr) { return prev.concat(arr); }, vars)
+        );
+      } else if (typeof ast[k] === 'object' && ast[k] !== null) {
+        return prev.concat(extract(ast[k], vars));
+      } else {
+        return prev;
+      }
+    }, []);
+  }
+
+  var vars = extract(ast, []);
+  return vars.filter(function(v, i){ return vars.indexOf(v) >= i; });
 }
 
 function correctOne(transAst, paramMap, trace, correction) {
@@ -264,7 +236,6 @@ function srtr(transAst, paramMap, trace, corrections) {
 }
 
 module.exports = {
-  astRemoveNode: astRemoveNode,
   astToJS: astToJS,
   subsituteVariables: subsituteVariables,
   extractVariables: extractVariables,
--- test/index.test.js
+++ test/index.test.js
@@ -1,300 +1,11 @@
 const {parser} = require('js2smt2');
 const {
-  astRemoveNode,
   astToJS,
-  makeResidual,
   subsituteVariables,
+  makeResidual,
   extractVariables,
   correctOne,
 } = require('../src/');
-
-test('astRemoveNode', () => {
-  const ast = parser.parse(`
-function f(a, b) {
-  if (a == 'hello' && b.type == 'there' && b.value * 1 === 0) {
-    return {
-      state: 'branch1',
-      outputs: {
-        action1: 'a1',
-        action2: 100,
-      },
-    };
-  } else if (a == 'jello' && b.type == 'whirl' && b.value + 1 === 2) {
-    return {
-      state: 'branch2',
-      outputs: {
-        action1: 'a2',
-        action2: 200,
-      },
-    };
-  } else {
-    return {
-      state: 'branch3',
-      outputs: {
-        action1: 'a3',
-        action2: 300,
-      },
-    };
-  }
-}
-`);
-
-  const filtered = astRemoveNode(
-    ast,
-    function (ast) {
-      return !(
-        ast.type === 'Property'
-        && ast.key.type === 'Identifier'
-        && ast.key.name === 'outputs'
-      );
-    },
-  );
-
-  expect(filtered).toEqual({
-    "type": "Program",
-    "body": [
-        {
-          "type": "FunctionDeclaration",
-          "id": {
-              "type": "Identifier",
-              "name": "f"
-          },
-          "params": [
-              {
-                "type": "Identifier",
-                "name": "a"
-              },
-              {
-                "type": "Identifier",
-                "name": "b"
-              }
-          ],
-          "body": {
-              "type": "BlockStatement",
-              "body": [
-                {
-                    "type": "IfStatement",
-                    "test": {
-                      "type": "LogicalExpression",
-                      "operator": "&&",
-                      "left": {
-                          "type": "LogicalExpression",
-                          "operator": "&&",
-                          "left": {
-                            "type": "BinaryExpression",
-                            "operator": "==",
-                            "left": {
-                                "type": "Identifier",
-                                "name": "a"
-                            },
-                            "right": {
-                                "type": "Literal",
-                                "value": "hello"
-                            }
-                          },
-                          "right": {
-                            "type": "BinaryExpression",
-                            "operator": "==",
-                            "left": {
-                                "type": "MemberExpression",
-                                "object": {
-                                  "type": "Identifier",
-                                  "name": "b"
-                                },
-                                "property": {
-                                  "type": "Identifier",
-                                  "name": "type"
-                                },
-                                "computed": false
-                            },
-                            "right": {
-                                "type": "Literal",
-                                "value": "there"
-                            }
-                          }
-                      },
-                      "right": {
-                          "type": "BinaryExpression",
-                          "operator": "===",
-                          "left": {
-                            "type": "BinaryExpression",
-                            "operator": "*",
-                            "left": {
-                                "type": "MemberExpression",
-                                "object": {
-                                  "type": "Identifier",
-                                  "name": "b"
-                                },
-                                "property": {
-                                  "type": "Identifier",
-                                  "name": "value"
-                                },
-                                "computed": false
-                            },
-                            "right": {
-                                "type": "Literal",
-                                "value": 1
-                            }
-                          },
-                          "right": {
-                            "type": "Literal",
-                            "value": 0
-                          }
-                      }
-                    },
-                    "consequent": {
-                      "type": "BlockStatement",
-                      "body": [
-                          {
-                            "type": "ReturnStatement",
-                            "argument": {
-                                "type": "ObjectExpression",
-                                "properties": [
-                                  {
-                                      "type": "Property",
-                                      "key": {
-                                        "type": "Identifier",
-                                        "name": "state"
-                                      },
-                                      "value": {
-                                        "type": "Literal",
-                                        "value": "branch1"
-                                      },
-                                      "kind": "init"
-                                  },
-                                ]
-                            }
-                          }
-                      ]
-                    },
-                    "alternate": {
-                      "type": "IfStatement",
-                      "test": {
-                          "type": "LogicalExpression",
-                          "operator": "&&",
-                          "left": {
-                            "type": "LogicalExpression",
-                            "operator": "&&",
-                            "left": {
-                                "type": "BinaryExpression",
-                                "operator": "==",
-                                "left": {
-                                  "type": "Identifier",
-                                  "name": "a"
-                                },
-                                "right": {
-                                  "type": "Literal",
-                                  "value": "jello"
-                                }
-                            },
-                            "right": {
-                                "type": "BinaryExpression",
-                                "operator": "==",
-                                "left": {
-                                  "type": "MemberExpression",
-                                  "object": {
-                                      "type": "Identifier",
-                                      "name": "b"
-                                  },
-                                  "property": {
-                                      "type": "Identifier",
-                                      "name": "type"
-                                  },
-                                  "computed": false
-                                },
-                                "right": {
-                                  "type": "Literal",
-                                  "value": "whirl"
-                                }
-                            }
-                          },
-                          "right": {
-                            "type": "BinaryExpression",
-                            "operator": "===",
-                            "left": {
-                                "type": "BinaryExpression",
-                                "operator": "+",
-                                "left": {
-                                  "type": "MemberExpression",
-                                  "object": {
-                                      "type": "Identifier",
-                                      "name": "b"
-                                  },
-                                  "property": {
-                                      "type": "Identifier",
-                                      "name": "value"
-                                  },
-                                  "computed": false
-                                },
-                                "right": {
-                                  "type": "Literal",
-                                  "value": 1
-                                }
-                            },
-                            "right": {
-                                "type": "Literal",
-                                "value": 2
-                            }
-                          }
-                      },
-                      "consequent": {
-                          "type": "BlockStatement",
-                          "body": [
-                            {
-                                "type": "ReturnStatement",
-                                "argument": {
-                                  "type": "ObjectExpression",
-                                  "properties": [
-                                      {
-                                        "type": "Property",
-                                        "key": {
-                                            "type": "Identifier",
-                                            "name": "state"
-                                        },
-                                        "value": {
-                                            "type": "Literal",
-                                            "value": "branch2"
-                                        },
-                                        "kind": "init"
-                                      },
-                                  ]
-                                }
-                            }
-                          ]
-                      },
-                      "alternate": {
-                          "type": "BlockStatement",
-                          "body": [
-                            {
-                                "type": "ReturnStatement",
-                                "argument": {
-                                  "type": "ObjectExpression",
-                                  "properties": [
-                                      {
-                                        "type": "Property",
-                                        "key": {
-                                            "type": "Identifier",
-                                            "name": "state"
-                                        },
-                                        "value": {
-                                            "type": "Literal",
-                                            "value": "branch3"
-                                        },
-                                        "kind": "init"
-                                      },
-                                  ]
-                                }
-                            }
-                          ]
-                      }
-                    }
-                }
-              ]
-          }
-        }
-    ]
-  });
-});
 
 test('astToJS', () => {
   const ast = parser.parse(`
@@ -366,13 +77,6 @@ test('subsituteVariables - MemberExpression', () => {
   const astSubed = subsituteVariables(ast, varMap);
   expect(astToJS(astSubed))
     .toBe(`"there"`);
-});
-
-test('extractVariables', () => {
-  const ast = parser.parse(
-      `a == 'hello' && b.type == 'there' && b.value * 1 === 0`);
-  const variables = extractVariables(ast);
-  expect(variables.sort()).toEqual(["a", "b"]);
 });
 
 test('makeResidual', () => {
@@ -460,7 +164,7 @@ if (state == 'A' && b.value < 1) {
   });
 });
 
-test('makeResidual - two states', () => {
+test('makeResidual - two "from" states', () => {
   const transAst = parser.parse(`
 if (state == 'A' && b.value < paramA) {
   return 'A';
@@ -527,6 +231,13 @@ if (state == 'A' && b.value < paramA) {
   });
 });
 
+test('extractVariables', () => {
+  const ast = parser.parse(
+      `a == 'hello' && b.type == 'there' && b.value * 1 === 0`);
+  const variables = extractVariables(ast);
+  expect(variables.sort()).toEqual(["a", "b"]);
+});
+
 test('correctOne', () => {
   const transAst = parser.parse(`
 if (state == 'A' && b.value < 1) {
@@ -548,7 +259,7 @@ if (state == 'A' && b.value < 1) {
 
   const correctedAst = correctOne(transAst, parameterMap, trace, correction);
 
-  console.log(JSON.stringify(correctedAst, null, 2));
+  // console.log(JSON.stringify(correctedAst, null, 2));
 
   expect(false).toBe(true);
 });
