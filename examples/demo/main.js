9dafe3356df5501494d458f203febdf9636e103f
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-04-24 13:34:26 -0700

    Update demo to run srtr example with z3

--- examples/demo/main.js
+++ examples/demo/main.js
@@ -1,36 +1,67 @@
 const spawn = require('child_process').spawn;
 const {interpret, parser} = require('js2smt2');
+const {createSRTRSMT2} = require('../../');
 
-const JS_CODE = `
-function f(a, b) {
-  if (a == 'hello' && b.type == 'there' && b.value * 1 === 0) {
-    return 'branch1';
-  } else if (a == 'jello' && b.type == 'whirl' && b.value + 1 === 2) {
-    return 'branch2';
-  } else {
-    return 'branch3';
-  }
+const transAst = parser.parse(`
+if (state == 'A' && b.value > paramA) {
+  return 'B';
+} else if (state == 'B' && b.value <= paramA) {
+  return 'A';
+} else {
+  return state;
 }
-`;
+`);
 
-const typeDefs = {
-  f: {
-    a: 'String',
-    b: '(B String Int)',
-    return: 'String',
+const parameterMap = {
+  paramA: 2,
+};
+
+const traces = [
+  {
+    timestamp: 0,
+    trace: {
+      state: 'A',
+      b: {value: 1},
+    }
+  },
+  {
+    timestamp: 1,
+    trace: {
+      state: 'B',
+      b: {value: -1},
+    }
   }
-}
+];
 
-const z3Input = `
-(declare-datatypes (T1 T2) ((B (mk-input (type T1) (value T2)))))
-${interpret(parser.parse(JS_CODE).body[0], typeDefs)}
+const corrections = [
+  {
+    timestamp: 0,
+    correction: 'B'
+  },
+  {
+    timestamp: 1,
+    correction: 'A'
+  }
+];
 
-(declare-const x String)
-(declare-const y (B String Int))
-(assert (= (f x y) "branch2"))
-(check-sat)
-(get-model)
-`;
+const options = {H: 1};
+
+const z3Input = createSRTRSMT2(
+    transAst, parameterMap, traces, corrections, options
+  ) + '\n(check-sat) (get-model)';
+
+// console.log(z3Input);
+
+// const z3Input = `
+// (declare-datatypes (T1 T2) ((B (mk-input (type T1) (value T2)))))
+// ${interpret(parser.parse(JS_CODE).body[0], typeDefs)}
+
+// (declare-const x String)
+// (declare-const y (B String Int))
+// (assert (= (f x y) "branch2"))
+// (check-sat)
+// (get-model)
+// `;
 
 const p = spawn('z3', ['-T:5', '-smt2', '-in'], {stdio: ['pipe', 'pipe', 'ignore']});
 p.stdin.write(z3Input);
